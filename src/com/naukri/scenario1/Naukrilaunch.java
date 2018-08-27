ba7bd9e416f641fcd9d3f3865957ef8329eb5ae0
Jayamala <Jayamala@Jayamala-PC>
2018-08-27 16:27:16 +0530

    batch1

---
+++ src/com/naukri/scenario1/Naukrilaunch.java
@@ -0,0 +1,43 @@
+package com.naukri.scenario1;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Naukrilaunch {
+	public static WebDriver driver=null;
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\SeleniumAutomation\\Drivers\\chromedriver.exe");
+		
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.navigate().to("https://www.naukri.com/");
+		
+
+		String mainwindow=driver.getWindowHandle();
+		System.out.println("main window-->" +mainwindow);
+		
+		Set<String> allwindowhandle=driver.getWindowHandles();
+		
+		         
+		for(String handle:allwindowhandle)
+		
+		{
+			if(handle.equals(mainwindow))
+				continue;
+			
+			else 
+			{
+				String URL=driver.switchTo().window(handle).getCurrentUrl();
+			System.out.println("Switching to window - > " + URL);
+			driver.close();
+			}
+					
+		}
+
+	}
+
+}
